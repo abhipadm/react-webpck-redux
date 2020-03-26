6a0f94ca9a502c075374aefb1ba75d836483ee1a
Abhijith <abhijth@hotmail.com>
2020-03-26 13:32:34 +0530

    routing navigation header and some pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,16 +3,20 @@ import { Switch, Route } from "react-router-dom";
 
 import HomePage from "./home/HomePage";
 import AboutPage from "./about/AboutPage";
+import Header from "./shared/Header";
+import PageNotFound from "./PageNotFound";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="container-fluid">
-      <switch>
-        <Route exact path="/" component={HomePage} />{" "}
-        <Route exact path="/about" component={AboutPage} />{" "}
-      </switch>{" "}
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/about" component={AboutPage} />
+        <Route component={PageNotFound} />
+      </Switch>
     </div>
   );
 }
